916d5d32108bbda56bc2cf4b5883d328d45128f7
Christian Parrish <a01751584@itesm.mx>
2022-03-24 18:29:48 -0600

    Versión completa final

--- paint.py
+++ paint.py
@@ -1,12 +1,8 @@
 """Paint, for drawing shapes.
 
-Exercises
-
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
+authors:
+Paulo Ogando Gulias                  A01751587
+Christian Parrish Gutiérrez Arrieta  A01751584
 """
 
 from turtle import *
